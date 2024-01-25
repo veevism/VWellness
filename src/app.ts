748b228ff05bd0fd4291a52d5b86519ab12b3579
sorawee <veevisorawee@gmail.com>
2024-01-26 05:40:21 +0700

    setting log format

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
+import expressWinston from "express-winston";
+import winston from "winston";
+
+import logger from "./config/logger";
 
 class App {
   public app: express.Application;
@@ -13,6 +17,24 @@ class App {
   private setMiddlewares(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    const logFormat = winston.format.printf(
+      ({ level, message, timestamp, meta }) => {
+        return `${timestamp} [${level}] ${message} ${
+          meta ? JSON.stringify(meta) : ""
+        }`;
+      }
+    );
+    this.app.use(
+      expressWinston.logger({
+        winstonInstance: logger,
+        format: winston.format.combine(winston.format.timestamp(), logFormat),
+        msg: "{{req.method}} {{req.url}} - HTTP {{res.statusCode}}",
+        expressFormat: false,
+        colorize: false,
+        statusLevels: true,
+        meta: false, // ไม่ต้องแสดง meta แบบมากเหมือนเดิม
+      })
+    );
   }
 
   private setRoutes(): void {
