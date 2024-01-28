0512939e731b4dd7f6fcb90bdd7df6e5864fe12e
sorawee <veevisorawee@gmail.com>
2024-01-29 01:50:08 +0700

    *Bug Fixed* Next Func can't be alone
    Took 10 minutes

--- src/api/middleware/routeNotFound.ts
+++ src/api/middleware/routeNotFound.ts
@@ -1,7 +1,7 @@
-import {NextFunction } from 'express';
+import {NextFunction, Request, Response} from 'express';
 import {GeneralError} from "../../util/helper";
 
-export function routeNotFound(next: NextFunction) {
+export function routeNotFound(req: Request, res: Response, next: NextFunction) {
     next(new GeneralError(404, "Resource not found"));
 
 }
